5fc4037848550e4e477a5d0e3455acf14e7f536a
Matthew Casperson <matthewcasperson@gmail.com>
2017-10-30 16:43:49 +1000

    Added some comments

--- source/Octopus.Server.Core.Versioning/Octopus/Core/Resources/Metadata/IPackageIDParser.cs
+++ source/Octopus.Server.Core.Versioning/Octopus/Core/Resources/Metadata/IPackageIDParser.cs
@@ -27,15 +27,21 @@
         /// <returns>The metadata assocaited with the package id</returns>
         PackageMetadata GetMetadataFromPackageID(string packageID, string version, string extension);
         /// <summary>
-        /// Extracts metadata from a package file name
+        /// The target Files cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre.jar-e55fcd51-6081-4300-91a3-117b7930c023" or
+        /// "com.google.guava#guava#23.3-jre.jar" or
+        /// "mypackage.1.0.0.0.nuget-f363ce3a-0657-401a-8831-f3634f6cca2b".
+        /// This method will break down these filenames.
         /// </summary>
         /// <param name="packageFile">The package file name</param>
         /// <param name="extensions">The extensions that this parser should know about</param>
         /// <returns>The metadata assocaited with the package file</returns>
         PackageMetadata GetMetadataFromPackageName(string packageFile, string[] extensions);
         /// <summary>
-        /// The server side cache has filenames like "com.google.guava#guava#23.3-jre_9822965F2883AD43AD79DA4E8795319F.jar"
-        /// which required some different parsing.
+        /// The server side cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre_9822965F2883AD43AD79DA4E8795319F.jar" or
+        /// "mypackage.1.0.0.0_BA90F59B8C9EE04DADE2D3501181EFCD.nuget".
+        /// This method will break down this filename.
         /// </summary>
         /// <param name="packageFile">The package file name</param>
         /// <param name="extensions">The extensions that this parser should know about</param>
