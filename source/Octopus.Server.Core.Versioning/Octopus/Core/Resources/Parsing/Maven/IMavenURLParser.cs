910ddfdb62a15c635a9ee84547d4a118ac366ddc
Matthew Casperson <matthewcasperson@gmail.com>
2017-11-03 12:18:24 +1000

    Added a service for sanitising maven uris

---
+++ source/Octopus.Server.Core.Versioning/Octopus/Core/Resources/Parsing/Maven/IMavenURLParser.cs
@@ -0,0 +1,13 @@
+﻿namespace Octopus.Core.Resources.Parsing.Maven
+{
+    public interface IMavenURLParser
+    {
+        /// <summary>
+        /// Given a maven repo, return a sanitised uri that we can use
+        /// to download packages.
+        /// </summary>
+        /// <param name="uri">The input uri</param>
+        /// <returns>The sanitised uri</returns>
+        string SanitiseFeedUri(string uri);
+    }
+}
---
+++ source/Octopus.Server.Core.Versioning/Octopus/Core/Resources/Parsing/Maven/MavenURLParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Octopus.Core.Resources.Parsing.Maven
+{
+    public class MavenURLParser : IMavenURLParser
+    {
+        public string SanitiseFeedUri(string uri) => Regex.Replace(uri, "(maven2)?//$", "");
+    }
+}
