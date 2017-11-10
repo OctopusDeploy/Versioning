bea8873ac7745b27b112cd388d52b0372e293f36
Matthew Casperson <matthewcasperson@gmail.com>
2017-11-10 11:57:11 +1000

    Added more tests

---
+++ source/Octopus.Server.Core.Versioning.Tests/Versions/CustomRangeTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Octopus.Core.Resources.Ranges;
+using Octopus.Core.Resources.Versioning.Maven;
+
+namespace Octopus.Server.Core.Versioning.Tests.Versions
+{
+    [TestFixture]
+    public class CustomRangeTests
+    {
+        [Test]
+        public void testVersionsWithQualifiers()
+        {
+            MavenVersionRange range = MavenVersionRange.CreateFromVersionSpec("[1.5,20)");
+            Assert.IsFalse(range.ContainsVersion(new MavenVersionParser().Parse("23.4-jre")));
+            Assert.IsTrue(range.ContainsVersion(new MavenVersionParser().Parse("19.4-jre")));
+        }
+    }
+}
