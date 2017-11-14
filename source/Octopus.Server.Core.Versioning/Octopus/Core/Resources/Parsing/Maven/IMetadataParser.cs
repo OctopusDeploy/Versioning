eca14a7b11ac8400d5d6ce5821fe64900a837db5
Matthew Casperson <matthewcasperson@gmail.com>
2017-11-14 10:15:29 +1000

    Added a common routine for parsing metadata xml files

---
+++ source/Octopus.Server.Core.Versioning/Octopus/Core/Resources/Parsing/Maven/IMetadataParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml;
+
+namespace Octopus.Core.Resources.Parsing.Maven
+{
+    /// <summary>
+    /// Defines a service for parsing metadata files
+    /// </summary>
+    public interface IMetadataParser
+    {
+        /// <summary>
+        /// Given a maven-metadata.xml file parsed into a XML document, find the latest
+        /// value of a snapshot artifact for a the given extension.
+        /// </summary>
+        /// <param name="snapshotMetadata">The parsed Snapshot maven-metadata.xml file</param>
+        /// <param name="extension">The artifect extension</param>
+        /// <returns></returns>
+        string GetLatestSnapshotRelease(XmlDocument snapshotMetadata, string extension);
+    }
+}
---
+++ source/Octopus.Server.Core.Versioning/Octopus/Core/Resources/Parsing/Maven/MetadataParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using Octopus.Core.Extensions;
+
+namespace Octopus.Core.Resources.Parsing.Maven
+{
+    public class MetadataParser : IMetadataParser
+    {
+        public string GetLatestSnapshotRelease(XmlDocument snapshotMetadata, string extension)
+        {
+            return snapshotMetadata.ToEnumerable()
+                       .Select(doc => doc.DocumentElement?.SelectSingleNode("./*[local-name()='versioning']"))
+                       .Select(node => node?.SelectNodes("./*[local-name()='snapshotVersions']/*[local-name()='snapshotVersion']"))
+                       .Where(nodes => nodes != null)
+                       .SelectMany(nodes => nodes.Cast<XmlNode>())
+                       .Where(node => (node.SelectSingleNode("./*[local-name()='extension']")?.InnerText.Trim() ?? "").Equals(extension.Trim(), StringComparison.OrdinalIgnoreCase))
+                       .OrderByDescending(node => node.SelectSingleNode("./*[local-name()='updated']")?.InnerText)
+                       .Select(node => node.SelectSingleNode("./*[local-name()='value']")?.InnerText)
+                       .FirstOrDefault() ?? "";
+        }
+    }
+}
