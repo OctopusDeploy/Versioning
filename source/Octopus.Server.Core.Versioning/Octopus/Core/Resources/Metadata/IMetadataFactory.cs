072a851b0940f89f25bdd9b51bf857ab44e54fdf
Matthew Casperson <matthewcasperson@gmail.com>
2017-11-23 12:25:58 +1000

    Created a metadata factory

---
+++ source/Octopus.Server.Core.Versioning/Octopus/Core/Resources/Metadata/IMetadataFactory.cs
@@ -0,0 +1,149 @@
+﻿using Octopus.Core.Util;
+
+namespace Octopus.Core.Resources.Metadata
+{
+    public interface IMetadataFactory
+    {
+        /// <summary>
+        /// Extracts metadata from a package ID (i.e. no version information)
+        /// </summary>
+        /// <param name="packageID">The package id</param>
+        /// <returns>The metadata assocaited with the package id</returns>
+        BasePackageMetadata GetMetadataFromPackageID(string packageID, FeedType feedType);
+        /// <summary>
+        /// Returns true or false based on whether or not we can parse the
+        /// supplied package id.
+        /// </summary>
+        /// <param name="packageID">The package id</param>
+        /// <param name="metadata">The parsed metadata if we returned true</param>
+        /// <returns>True if this package id could be parsed</returns>
+        bool CanGetMetadataFromPackageID(string packageID, out BasePackageMetadata metadata, FeedType feedType);
+        /// <summary>
+        /// Extracts metadata from a package ID and adds the supplied version and extension
+        /// </summary>
+        /// <param name="packageID">The package id</param>
+        /// <param name="version">The package version</param>
+        /// <param name="extension">The package extension</param>
+        /// <returns>The metadata assocaited with the package id</returns>
+        PackageMetadata GetMetadataFromPackageID(string packageID, string version, string extension, FeedType feedType);
+        /// <summary>
+        /// Extracts metadata from a package ID and adds the supplied version and extension
+        /// </summary>
+        /// <param name="packageID">The package id</param>
+        /// <param name="version">The package version</param>
+        /// <param name="extension">The package extension</param>
+        /// <param name="size">The file size</param>
+        /// <param name="hash">The file hash</param>
+        /// <returns>The metadata assocaited with the package id</returns>
+        PhysicalPackageMetadata GetMetadataFromPackageID(string packageID, string version, string extension, long size, string hash, FeedType feedType);
+        /// <summary>
+        /// The target Files cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre.jar-e55fcd51-6081-4300-91a3-117b7930c023" or
+        /// "com.google.guava#guava#23.3-jre.jar" or
+        /// "mypackage.1.0.0.0.nuget-f363ce3a-0657-401a-8831-f3634f6cca2b".
+        /// This method will break down these filenames.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <param name="extensions">The extensions that this parser should know about</param>
+        /// <returns>The metadata assocaited with the package file</returns>
+        PackageMetadata GetMetadataFromPackageName(string packageFile, string[] extensions, FeedType feedType);
+        /// <summary>
+        /// The target Files cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre.jar-e55fcd51-6081-4300-91a3-117b7930c023" or
+        /// "com.google.guava#guava#23.3-jre.jar" or
+        /// "mypackage.1.0.0.0.nuget-f363ce3a-0657-401a-8831-f3634f6cca2b".
+        /// This method will break down these filenames.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <param name="extensions">The extensions that this parser should know about</param>
+        /// <param name="packageMetadata">The package metadata if the parsing was successful</param>
+        /// <returns>True if the file could be parsed, and false otherwise</returns>
+        bool CanGetMetadataFromPackageName(string packageFile, string[] extensions, out PackageMetadata packageMetadata, FeedType feedType);
+        /// <summary>
+        /// The target Files cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre.jar-e55fcd51-6081-4300-91a3-117b7930c023" or
+        /// "com.google.guava#guava#23.3-jre.jar" or
+        /// "mypackage.1.0.0.0.nuget-f363ce3a-0657-401a-8831-f3634f6cca2b".
+        /// This method will break down these filenames.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <param name="extensions">The extensions that this parser should know about</param>
+        /// <returns>A tuple with a boolean indicating the success of the parsing, and the metadata if parsing was successful</returns>
+        Maybe<PackageMetadata> CanGetMetadataFromPackageName(string packageFile, string[] extensions, FeedType feedType);
+        /// <summary>
+        /// The target Files cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre.jar-e55fcd51-6081-4300-91a3-117b7930c023" or
+        /// "com.google.guava#guava#23.3-jre.jar" or
+        /// "mypackage.1.0.0.0.nuget-f363ce3a-0657-401a-8831-f3634f6cca2b".
+        /// This method will break down these filenames.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <returns>A tuple with a boolean indicating the success of the parsing, and the metadata if parsing was successful</returns>
+        Maybe<PackageMetadata> CanGetMetadataFromPackageName(string packageFile, FeedType feedType);
+        /// <summary>
+        /// The server side cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre_9822965F2883AD43AD79DA4E8795319F.jar" or
+        /// "mypackage.1.0.0.0_BA90F59B8C9EE04DADE2D3501181EFCD.nuget".
+        /// This method will break down this filename.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <param name="extensions">The extensions that this parser should know about</param>
+        /// <returns>The metadata assocaited with the package file</returns>
+        PackageMetadata GetMetadataFromServerPackageName(string packageFile, string[] extensions, FeedType feedType);
+        /// <summary>
+        /// The server side cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre_9822965F2883AD43AD79DA4E8795319F.jar" or
+        /// "mypackage.1.0.0.0_BA90F59B8C9EE04DADE2D3501181EFCD.nuget".
+        /// This method will break down this filename.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <returns>The metadata assocaited with the package file</returns>
+        PackageMetadata GetMetadataFromServerPackageName(string packageFile, FeedType feedType);
+        /// <summary>
+        /// The server side cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre_9822965F2883AD43AD79DA4E8795319F.jar" or
+        /// "mypackage.1.0.0.0_BA90F59B8C9EE04DADE2D3501181EFCD.nuget".
+        /// This method will break down this filename.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <param name="extensions">The extensions that this parser should know about</param>
+        /// <param name="packageMetadata">The package metadata if the parsing was successful</param>
+        /// <returns>True if the file could be parsed, and false otherwise</returns>
+        bool CanGetMetadataFromServerPackageName(string packageFile, string[] extensions, out PackageMetadata packageMetadata, FeedType feedType);
+        /// <summary>
+        /// The server side cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre_9822965F2883AD43AD79DA4E8795319F.jar" or
+        /// "mypackage.1.0.0.0_BA90F59B8C9EE04DADE2D3501181EFCD.nuget".
+        /// This method will break down this filename.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <returns>A tuple with a boolean indicating the success of the parsing, and the metadata if parsing was successful</returns>
+        Maybe<PackageMetadata> CanGetMetadataFromServerPackageName(string packageFile, string[] extensions, FeedType feedType);
+        /// <summary>
+        /// The server side cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre_9822965F2883AD43AD79DA4E8795319F.jar" or
+        /// "mypackage.1.0.0.0_BA90F59B8C9EE04DADE2D3501181EFCD.nuget".
+        /// This method will break down this filename.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <returns>A tuple with a boolean indicating the success of the parsing, and the metadata if parsing was successful</returns>
+        Maybe<PackageMetadata> CanGetMetadataFromServerPackageName(string packageFile, FeedType feedType);
+        /// <summary>
+        /// The server side cache has filenames like 
+        /// "com.google.guava#guava#23.3-jre_9822965F2883AD43AD79DA4E8795319F.jar" or
+        /// "mypackage.1.0.0.0_BA90F59B8C9EE04DADE2D3501181EFCD.nuget".
+        /// This method will break down this filename.
+        /// </summary>
+        /// <param name="packageFile">The package file name</param>
+        /// <param name="extensions">The extensions that this parser should know about</param>
+        /// <param name="size">The file size</param>
+        /// <param name="hash">The file hash</param>
+        /// <returns>The metadata assocaited with the package file</returns>
+        PhysicalPackageMetadata GetMetadataFromServerPackageName(
+            string packageFile, 
+            string[] extensions,
+            long size,
+            string hash, 
+            FeedType feedType);
+    }
+}
---
+++ source/Octopus.Server.Core.Versioning/Octopus/Core/Resources/Metadata/MetadataFactory.cs
@@ -0,0 +1,112 @@
+﻿using Octopus.Core.Util;
+
+namespace Octopus.Core.Resources.Metadata
+{
+    public class MetadataFactory : IMetadataFactory
+    {
+        static readonly IPackageIDParser MavenPackageIdParser = new MavenPackageIDParser();
+        static readonly IPackageIDParser NugetPackageIdParser = new NuGetPackageIDParser();
+        
+        public BasePackageMetadata GetMetadataFromPackageID(string packageID, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.GetMetadataFromPackageID(packageID) : 
+                NugetPackageIdParser.GetMetadataFromPackageID(packageID);
+        }
+
+        public bool CanGetMetadataFromPackageID(string packageID, out BasePackageMetadata metadata, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.CanGetMetadataFromPackageID(packageID, out metadata) : 
+                NugetPackageIdParser.CanGetMetadataFromPackageID(packageID, out metadata);
+        }
+
+        public PackageMetadata GetMetadataFromPackageID(string packageID, string version, string extension, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ?
+                MavenPackageIdParser.GetMetadataFromPackageID(packageID, version, extension) : 
+                NugetPackageIdParser.GetMetadataFromPackageID(packageID, version, extension);
+        }
+
+        public PhysicalPackageMetadata GetMetadataFromPackageID(string packageID, string version, string extension, long size,
+            string hash, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.GetMetadataFromPackageID(packageID, version, extension, size, hash) : 
+                NugetPackageIdParser.GetMetadataFromPackageID(packageID, version, extension, size, hash);
+        }
+
+        public PackageMetadata GetMetadataFromPackageName(string packageFile, string[] extensions, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.GetMetadataFromPackageName(packageFile, extensions) : 
+                NugetPackageIdParser.GetMetadataFromPackageName(packageFile, extensions);
+        }
+
+        public bool CanGetMetadataFromPackageName(string packageFile, string[] extensions, out PackageMetadata packageMetadata,
+            FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.CanGetMetadataFromPackageName(packageFile, extensions, out packageMetadata) : 
+                NugetPackageIdParser.CanGetMetadataFromPackageName(packageFile, extensions, out packageMetadata);
+        }
+
+        public Maybe<PackageMetadata> CanGetMetadataFromPackageName(string packageFile, string[] extensions, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.CanGetMetadataFromPackageName(packageFile, extensions) : 
+                NugetPackageIdParser.CanGetMetadataFromPackageName(packageFile, extensions);
+        }
+
+        public Maybe<PackageMetadata> CanGetMetadataFromPackageName(string packageFile, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.CanGetMetadataFromPackageName(packageFile) : 
+                NugetPackageIdParser.CanGetMetadataFromPackageName(packageFile);
+        }
+
+        public PackageMetadata GetMetadataFromServerPackageName(string packageFile, string[] extensions, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.GetMetadataFromServerPackageName(packageFile, extensions) : 
+                NugetPackageIdParser.GetMetadataFromServerPackageName(packageFile, extensions);
+        }
+
+        public PackageMetadata GetMetadataFromServerPackageName(string packageFile, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.GetMetadataFromServerPackageName(packageFile) : 
+                NugetPackageIdParser.GetMetadataFromServerPackageName(packageFile);
+        }
+
+        public bool CanGetMetadataFromServerPackageName(string packageFile, string[] extensions, out PackageMetadata packageMetadata,
+            FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.CanGetMetadataFromServerPackageName(packageFile, extensions, out packageMetadata) : 
+                NugetPackageIdParser.CanGetMetadataFromServerPackageName(packageFile, extensions, out packageMetadata);
+        }
+
+        public Maybe<PackageMetadata> CanGetMetadataFromServerPackageName(string packageFile, string[] extensions, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.CanGetMetadataFromServerPackageName(packageFile, extensions) : 
+                NugetPackageIdParser.CanGetMetadataFromServerPackageName(packageFile, extensions);
+        }
+
+        public Maybe<PackageMetadata> CanGetMetadataFromServerPackageName(string packageFile, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.CanGetMetadataFromServerPackageName(packageFile) : 
+                NugetPackageIdParser.CanGetMetadataFromServerPackageName(packageFile);
+        }
+
+        public PhysicalPackageMetadata GetMetadataFromServerPackageName(string packageFile, string[] extensions, long size,
+            string hash, FeedType feedType)
+        {
+            return feedType == FeedType.Maven ? 
+                MavenPackageIdParser.GetMetadataFromServerPackageName(packageFile, extensions, size, hash) : 
+                NugetPackageIdParser.GetMetadataFromServerPackageName(packageFile, extensions, size, hash);
+        }
+    }
+}
