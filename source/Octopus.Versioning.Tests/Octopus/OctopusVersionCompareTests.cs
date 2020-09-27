bf9fd23b80e16ccd4b7bf1b1b1f7a159443a59b7
Matthew Casperson <matthewcasperson@gmail.com>
2020-09-28 09:14:43 +1000

    Renamed a test

--- source/Octopus.Versioning.Tests/Octopus/OctopusVersionCompareTests.cs
+++ source/Octopus.Versioning.Tests/Octopus/OctopusVersionCompareTests.cs
@@ -49,7 +49,7 @@ namespace Octopus.Versioning.Tests.Versions.Octopus
         [TestCase("1.0.0+meta", "1.0.0+meta", 0)]
         [TestCase("1.0.0+meta1", "1.0.0+meta2", -1)]
         [TestCase("1.0.0+meta3", "1.0.0+meta2", 1)]
-        public void TestSemverVersions(string version1, string version2, int result)
+        public void TestVersionComparisons(string version1, string version2, int result)
         {
             Assert.AreEqual(result, new OctopusVersionParser().Parse(version1).CompareTo(new OctopusVersionParser().Parse(version2)));
         }
