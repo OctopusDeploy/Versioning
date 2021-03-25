0adf8a0b1f1f8b302f117fc29b8afbc75c6f2458
Matthew Casperson <matthewcasperson@gmail.com>
2021-03-25 11:13:13 +1000

    Latest is no longer sorted any differently in docker tags

--- source/Octopus.Versioning/Docker/DockerTag.cs
+++ source/Octopus.Versioning/Docker/DockerTag.cs
@@ -43,33 +43,8 @@ namespace Octopus.Versioning.Docker
         {
         }
 
-        public override int Major => IsLatest ? int.MaxValue : base.Major;
-        public override int Minor => IsLatest ? int.MaxValue : base.Minor;
-        public override int Patch => IsLatest ? int.MaxValue : base.Patch;
-        public override int Revision => IsLatest ? int.MaxValue : base.Revision;
-
-        bool IsLatest => base.Major == 0 &&
-            base.Minor == 0 &&
-            base.Patch == 0 &&
-            base.Revision == 0 &&
-            base.ReleasePrefix == Latest &&
-            string.IsNullOrEmpty(base.ReleaseCounter) &&
-            string.IsNullOrEmpty(base.Metadata);
-
         public override VersionFormat Format => VersionFormat.Docker;
 
         public override bool IsPrerelease => !string.IsNullOrEmpty(Release) && OriginalString != Latest;
-
-        public override int CompareTo(object obj)
-        {
-            if (obj is IVersion objVersion)
-            {
-                if (OriginalString == Latest && objVersion.OriginalString == Latest) return 0;
-                if (OriginalString == Latest) return 1;
-                if (objVersion.OriginalString == Latest) return -1;
-            }
-
-            return base.CompareTo(obj);
-        }
     }
 }
